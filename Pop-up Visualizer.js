93bddac6959fcb31cd31ee2e43f6bbe315f7d93d
martingjeci <martingjeci@gmail.com>
2022-11-23 16:58:55 +0100

    Added Retrieve Members Api

--- Pop-up Visualizer.js
+++ Pop-up Visualizer.js
@@ -1,40 +1,39 @@
 
-function OpenPage () {
+function OpenPopUp () {
 
     var pageInput = {
+        //this can be: entity list, entity record, dashboard, HTML web resource or custom page
         pageType: "webresource",
-        webresourceName: "fi_Html_test"
+        //this is the logical name of the pageType we choose to open       
+        webresourceName: "fisoft_html"  
     };
 
     var navigationOptions = {
+        // target: 1 it opens the page inline , target: 2 it opens the page in a pop-up format
         target: 2,
+        //width of the pop-up   
         width: 400,
+        //height of the pop-up   
         height: 300,
-        position: 1
+        //position: 1 it opens the pop-up in the center of the screen, position: 2 opens the pop-up on the side of the screen
+        position: 1   
     };
 
+    //navigateTo is a build-in method by Microsoft
     Xrm.Navigation.navigateTo(pageInput, navigationOptions).then(
         function success() {
-            window.close();
             // Handle dialog closed
             console.log("Inside Success");
         },
         function error() {
             Xrm.Navigation.openAlertDialog({ text: error.message });
         }
-    );
-    
+    );   
 }
 
 
 
-function OpenPage () {
-    //Xrm.Utility.openWebResource("fi_Html_test");
-    //window.opener.Xrm.Page.openWebResource("fi_Html_test");
-    window.opener.Xrm.Page.getAttribute("fi_Html_test").getValue();
-}
 
-function download() {
-    var windowOptions = { height: 0, width: 0 };
-    Xrm.Navigation.openWebResource("fisoft_html_webresource",windowOptions);
-  }
+
+
+
---
+++ Retrieve Members Api.js
@@ -0,0 +1,52 @@
+var accountKey;
+
+async function retrieveTotalMembers(executionContext)
+{
+    var formContext = executionContext.getFormContext();
+
+    if (!accountKey) {
+		LoadSettings();
+	}
+	var segmentId = getSegmentId();
+    var url = `https://dev-eus-segmentation-api.azurewebsites.net/segment/totalrecords/${accountKey}/${segmentId}`;
+    var getMembers = await fetchMembers(url);
+    var totalMembers = getMembers.totalRecords;
+    formContext.getAttribute("cdi_members_count").setValue(totalMembers);
+}
+
+async function fetchMembers(url){
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        return data;
+    } catch (error) {
+        return console.log(error);
+    }
+}
+
+function LoadSettings() {
+	var globalContext = Xrm.Utility.getGlobalContext ? Xrm.Utility.getGlobalContext() : Xrm.Page.context;
+
+	var serverUrl = globalContext.getClientUrl();
+	var retrieveReq = new XMLHttpRequest();
+	retrieveReq.open('GET', serverUrl + '/webresources/cdi_settings', false);
+	retrieveReq.onreadystatechange = function () {
+		if (retrieveReq.readyState === 4 /* complete */) {
+			if (retrieveReq.status === 200) {
+				//Success
+				var xmlDoc = CreateXmlDoc(retrieveReq.responseText);
+
+				accountKey = SelectNodeValue(xmlDoc, 'accountkey');
+			}
+		}
+	};
+	retrieveReq.send();
+}
+
+function getSegmentId() {
+	var value = Xrm.Page.data.entity.getId();
+
+	var final_value = value.slice(value.indexOf('{') + 1, value.indexOf('}'));
+
+	return final_value.toLowerCase();
+}
